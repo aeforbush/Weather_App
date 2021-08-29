49531325e00779c8533c478383732d2efd78c8c7
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-29 12:33:09 -0600

    wrote html

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*var cities = [];
+
+
+
 var getWeather = function () {
 
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
@@ -11,8 +15,8 @@ var getWeather = function () {
     });
  
     
- // test
+ 
 
-}
+}*/
 
 
