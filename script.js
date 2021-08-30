38b55c903eecf4ae8efab38e0c611bbe2a738e21
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-30 11:03:41 -0600

    created spans for weather data

--- script.js
+++ script.js
@@ -35,8 +35,8 @@ var saveSearch = function() {
 
 // getCityWeather function
 var getWeather = function () {
-    var apiKey =  "f3c6f7687f7f43a162f3912305630533"
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}'
+    var apiKey =  'f3c6f7687f7f43a162f3912305630533';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={"apiKey"}';
 
     // make a request to the url for call back
     fetch(apiUrl)
@@ -44,8 +44,14 @@ var getWeather = function () {
         response.json().then(function(data) {
             displayWeather(data, city);
             console.log(response);
-        });
+        })
+        .catch(function() {
+            // catch any errors
+        })
     });
+    window.onload = function () {
+        getWeather();
+    }
 };
 // displayWeather function
 var displayWeather = function(weather, searchCity) {
@@ -53,14 +59,45 @@ var displayWeather = function(weather, searchCity) {
     weatherContainerEl.textContent= "";
     citySearchInputEl.textContent=searchCity;
 
-    console.log(weather);
+    //console.log(weather);
 
     // create date element
     var currentDate = document.createElement("span")
     currentDate.textContent=" (' + moment(weather.dt.value).format('MMMM Do YYYY, h:mm:ss a') + ') ";
     citySearchInputEl.appendChild(currentDate);
+
+    // create an image element
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("icon").scr = 'http://openweathermap.org/img/wn/10d@2x/${d.weather[0].icon}.png';
+    citySearchInputEl.appendChild(weatherIcon);
+    //console.log(weatherIcon);
+
+    // create span to hold temperature
+    var temperature = document.createElement("span")
+    temperature.textContent = "Temperature: " + current.weather.main.temp + " °F";
+    temperature.classList = "list-group-item"
+
+    // create span to hold humidity
+    var humidity = document.createElement("span")
+    humidity.textContent = "Humidity: " + current.weather.main.humidity + " %";
+    humidity.classList = "list-group-item"
+
+    // create span to hold wind speed
+    var windSpeed = document.createElement("span")
+    windSpeed.textContent = "Wind Speed: " + current.weather.main.speed + " MPH";
+    windSpeed.classList = "list-group-item"
+
+    // append to container
+    weatherContainerEl.appendChild(temperature);
+    weatherContainerEl.appendChild(humidity);
+    weatherContainerEl.appendChild(windSpeed);
+
 };
+
 // getUvIndex function
+/*var getUvIndex = function() {
+    var apiUrl = ''
+}*/
 // displayUvIndex function
 // getFiveDay function
 // displayFiveDay function
