c03c476fc0522913dbd0788f12938f44d4a5b7d2
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-31 19:35:21 -0600

    rm lat lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,36 @@
+var citiesArr = [];
 
+var cityInputEl = document.querySelector("#city");
+var cityBtn = document.querySelector("#btn");
+var cityNameEl = document.querySelector("#searched-city")
+var apiKey = 'f3c6f7687f7f43a162f3912305630533'
+var apiUrl = ''
+let log = console.log;
 
+var formHandler = function(event) {
+    log(event);
+    // get city name 
+    var searchedCity = cityInputEl.nodeValue.trim();
 
+    if (searchedCity) {
+        getLocCoords(searchedCity);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a city!");
+    };
+};
 
+// getcityName
+
+// get currentForecast with API
+var getCurrentForecast = function() {
+    fetch()
+}
+
+// display currentForecast
+
+// get fiveDay 
+
+// display fiveDay
+
+// no lat lon
