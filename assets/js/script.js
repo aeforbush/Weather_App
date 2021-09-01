748ff5180cb77111fd1c96c3d635147df2ce81f4
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-09-01 13:30:41 -0600

    current and five day forecasts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var cityInputEl = document.querySelector("#city");
 var cityBtn = document.querySelector("#btn");
 var cityNameEl = document.querySelector("#searched-city")
 var apiKey = 'f3c6f7687f7f43a162f3912305630533'
-var apiUrl = ''
 let log = console.log;
 
 var formHandler = function(event) {
@@ -13,7 +12,7 @@ var formHandler = function(event) {
     var searchedCity = cityInputEl.nodeValue.trim();
 
     if (searchedCity) {
-        getLocCoords(searchedCity);
+        getCityForecast(searchedCity);
         cityInputEl.value = "";
     } else {
         alert("Please enter a city!");
@@ -23,14 +22,90 @@ var formHandler = function(event) {
 // getcityName
 
 // get currentForecast with API
-var getCurrentForecast = function() {
-    fetch()
+var getCityForecast = function(city) {
+    var CurrentWeatherApi = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={apiKey}'
+    fetch(CurrentWeatherApi).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                getCityForecast(city);
+                
+                // saves searched city and refreshes recent city list
+                if (document.querySelector('.city-list')) {
+                    document.querySelector('.city-list').remove();
+                }
+
+                saveCity(city);
+                loadCities();
+                log(city);
+            })
+        } else {
+            alert('Error: ${response.statusText}')
+        }
+    })
+    .catch(function(error) {
+        alert('Unable to load weather!');
+    })
 }
+// get fiveDay 
+var getFiveDay = function(city) {
+    var CurrentWeatherApi = 'api.openweathermap.org/data/2.5/forecast?q={city}&appid={API key}'
+    fetch(CurrentWeatherApi).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                cityNameEl.textContent = `${city} (${moment().format("M/D/YYYY")})`;
+
+                log(data);
+                
+                currentForecast(data);
+                fiveDayForecast(data);
+
+            });
+        } 
+    })
+
+}
+
 
 // display currentForecast
 
-// get fiveDay 
+var currentForecast = function(forecast) {
+
+    var forecasteEl = document.querySelector('.city-forecast');
+    forecasteEl.classList.remove('hide');
+
+    var weatherIconEl = document.querySelector('#icon');
+    var currentIcon = forecast.currentForecast.weather[0].icon;
+    weatherIconEl.setAttribute('src', 'http://openweathermap.org/img/wn/${currentIcon}.png');
+
+    displayTemp('#current-temp', forecast.current['temp']);
+
+    var currentHumidityEl = document.querySelector('#current-humidity');
+    currentHumidityEl.textContent = forecast.current['humidity'];
+
+    var currentWindEl = document.querySelector('#current-wind-speed');
+    currentWindEl.textContent = forecast.current['wind-speed'];
+
+    var UvEl = document.querySelector('#current-uv');
+    var currentUv = forecast.current['uv'];
+    UvEl.textcontent = currentUv;
+}
 
 // display fiveDay
+var fiveDayForecast = function(forecast) {
+    for (var i = 0; i < 6; i++) {
+        var dateP = document.querySelector('#date-' + i);
+        dateP.textContent = moment().add(i, 'days').format('M/D/YYYY');
+
+        var iconImg = document.querySelector('#icon-' + i);
+        var iconCode = forecast.daily[i].weather[0].icon;
+        iconImg.setAttribute('scr', 'http://openweathermap.org/img/wn/${iconCode}.png');
+
+        displayTemp('#temp-' + i, forecast.daily[i].temp.day);
+
+        var humiditySpan = document.querySelector('#humidty-' + i);
+        humiditySpan.textContent = forecast.daily[i].humidity;
+    }
+
+}
 
 // no lat lon
